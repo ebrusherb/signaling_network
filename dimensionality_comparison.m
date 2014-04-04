dd0cc843fd3e1914ab84ecabeaddda749d46e176
ebrusherb <eleanor.brush@gmail.com>
2014-04-04 15:47:58 -0400

    figures for 1d vs 2d comparison

--- dimensionality_comparison.m
+++ dimensionality_comparison.m
@@ -3,6 +3,16 @@ blue=importdata('blue.txt');
 red=importdata('red.txt');
 black=importdata('black.txt');
 %%
+axislw=1.25;
+lw=3;
+labfontsz=15 ;
+textfontsz=15;
+alpha=.7;
+ticklength=[.02,.0250];
+markersz=5;
+markercol='white';
+
+%%
 deltax1=0.01;
 deltax2=0.025 ;
 b=1;
@@ -44,17 +54,7 @@ end
 filename=strcat('two_v_one_output','.mat');
 save(filename,'twooutput','oneoutput','domvals','lvals','threshvals1','threshvals2');
 %%
-axislw=1.25;
-lw=3;
-labfontsz=15 ;
-textfontsz=15;
-alpha=.7;
-ticklength=[.02,.0250];
-markersz=5;
-markercol='white';
-rho=4.2/5.6;
-w=8.5;
-h=w*ratio;
+
 figure
 set(gcf,'Color','w')
 v=get(gcf,'Position');
@@ -65,9 +65,8 @@ set(gcf,'Units','inches');
 set(gcf,'Position',[v(1) v(2) w h]);
 
 textlabels={'a' 'b'};
-increment=floor(size(blue,1)/Nd);
 
-p=[];
+p=zeros(1,2*Nd);
 
 for k=1:2
     subplot_tight(1,4,(1:2)+2*(k-1),[.1 .07])
@@ -77,7 +76,7 @@ for k=1:2
         case 2
             for i=2:Nd
             p(i)=plot(reshape(twooutput(k,i,:,2),1,[]),reshape(twooutput(k,i,:,1),1,[]),'Color',blue(i,:),'LineWidth',lw);
-            p(i+Nd)=plot(reshape(oneoutput(k,i,1:40,2),1,[]),reshape(oneoutput(k,i,1:40,1),1,[]),'Color',red(i,:),'LineWidth',lw)
+            p(i+Nd)=plot(reshape(oneoutput(k,i,1:40,2),1,[]),reshape(oneoutput(k,i,1:40,1),1,[]),'Color',red(i,:),'LineWidth',lw);
             end
         otherwise 
             for i=2:Nd
@@ -92,7 +91,7 @@ end
 
 leg=legend([p(Nd) p(Nd+Nd) p(Nd+(Nd:-1:2))],'Two dimensions','One dimension',['d=' num2str(domvals(Nd))],['d=' num2str(domvals(Nd-1))],['d=' num2str(domvals(Nd-2))],['d=' num2str(domvals(Nd-3))],['d=' num2str(domvals(Nd-4))]);
 legend boxoff
-leg_line=findobj(leg,'type','Line')
+leg_line=findobj(leg,'type','Line');
 for i = 1:5
      set(leg_line(2*i), 'Color', black(i+1,:));
      set(leg_line(2*i-1),'Color',black(i+1,:));
@@ -105,138 +104,71 @@ set(gcf,'PaperPosition',[0 0 w h]);
 
 filename=strcat('/Users/eleanorbrush/Dropbox/signaling_network/','1d_v_2d_symmetric','.pdf');
 print(filename,'-dpdf','-r300');
-%%
-axislw=1.25;
-lw=3;
-labfontsz=15 ;
-textfontsz=15;
-alpha=.7;
-ticklength=[.02,.0250];
-markersz=5;
-markercol='white';
-rho=4.2/5.6;
-w=8.5;
-h=w*rho;
-
-figure
-    set(gcf,'Color','w')
-    v=get(gcf,'Position');
-    ratio=v(4)/v(3);
-    w=6.83*2;
-    h=w*ratio;
-    
-
-for j=1:2
-    subplot(1,2,j)
-    k=j;
-    for i=2:Nd
-    plot(reshape(twooutput(k,i,:,1),1,[]),reshape(twooutput(k,i,:,2),1,[]),'blue','LineWidth',lw)
-    hold on
-    plot(reshape(oneoutput(k,i,1:40,1),1,[]),reshape(oneoutput(k,i,1:40,2),1,[]),'red','LineWidth',lw)
-    end
-    xlabel('Probability of correct output','FontSize',textfontsz)
-    ylabel('Expected time to output','FontSize',textfontsz)
-
-end
-%%
-axislw=1.25;
-lw=3;
-labfontsz=15 ;
-textfontsz=15;
-alpha=.7;
-ticklength=[.02,.0250];
-markersz=5;
-markercol='white';
-rho=4.2/5.6;
-w=8.5;
-h=w*rho;
-
-for k=Nl:-1:1
-    figure
-    set(gcf,'Color','w')
-    v=get(gcf,'Position');
-    ratio=v(4)/v(3);
-    w=6.83*2;
-    h=w*ratio;
-    hold on
-    for i=2:Nd
-        d=domvals(i);
-        if d==1
-            Hfight=0;
-        else
-            Hfight=-(d*log(d)+(1-d)*log(1-d));
-        end
-        twop=reshape(twooutput(k,i,:,1),1,[]);
-        Hsig_two=-(twop.*log(twop)+(1-twop).*log(1-twop));
-        plot(Hsig_two,reshape(twooutput(k,i,:,2),1,[]),'Color',(1-.1*i)*[0 0 1],'LineWidth',lw)
-        onep=reshape(oneoutput(k,i,1:40,1),1,[]);
-        Hsig_one=-(onep.*log(onep)+(1-onep).*log(1-onep));
-        plot(Hsig_one,reshape(oneoutput(k,i,1:40,2),1,[]),'Color',(1-.1*i)*[1 0 0],'LineWidth',lw)
-    end
-    xlabel('Probability of correct output','FontSize',textfontsz)
-    ylabel('Expected time to output','FontSize',textfontsz)
-%     leg=legend('Two dimensions','One dimension');
-%     set(leg,'Position',[.6 .8 .1 .05])
-end
-
-
-%%
-x=.5:.01:1;
-Nx=length(x);
-v=zeros(Nl,Nd-1,Nx);
-
-for k=1:Nl
-    for i=2:Nd
-        v(k,i,:)=interp1(reshape(oneoutput(k,i,1:40,1),1,[]),reshape(oneoutput(k,i,1:40,2),1,[]),.5:.01:1);
-    end
-end
 
 %%
 l=lvals(2);
 d=domvals(end-1);
 
-threshvals2=.1:.25:2;
-Nt2=length(threshvals2);
+threshvals=.1:.1:2;
+Nt=length(threshvals);
 
-twomat=zeros(Nt2,Nt2,2);
+ratios=[.1 .5 1 2 10];
+Nr=length(ratios);
 
-for i=1:Nt2
-    T1=threshvals2(i);
-    for j=1:Nt2
-        T2=threshvals2(j);
+twoflexthresh=zeros(Nr,Nt,2);
+
+for i=1:Nr
+    r=ratios(i);
+    for j=1:Nt
+        T1=threshvals(j);
+        T2=r*T1;
         [x,y,~]=solve_pde_2d(-T1,-T2,d,5,5,deltax2,deltax2,l,b);
-        twomat(i,j,:)=[x y];
+        twoflexthresh(i,j,:)=[x y];
     end
 end
 
-symm=diag(twomat(:,:,1));
-[L,~]=contour(threshvals2,threshvals2,transpose(twomat(:,:,1)),.5:.05:1,'LineWidth',3);
-L=dividecontours(L); 
-numlines=length(L);
-
-M=cell(1,numlines);
-
-for i=1:numlines
-    go=size(L{i},2);
-    M{i}=zeros(go,4);
-    for j=1:go
-        T1=L{i}(1,j);
-        T2=L{i}(2,j);
-        [x,y,~]=solve_pde_2d(-T1,-T2,d,5,5,deltax2,deltax2,l,b);
-        M{i}(j,:)=[T1,T2,x,y];
-    end
-end
-
-for i=1:4
-    [~,v]=sort(M{i}(:,2));
-    M{i}=M{i}(v,:);
-end
 
 %%
-contour(threshvals2,threshvals2,transpose(twomat(:,:,1)),0:.05:1,'LineWidth',3);
+figure
+set(gcf,'Color','w')
+v=get(gcf,'Position');
+ratio=v(4)/v(3);
+w=6.83*2;
+h=w/2;
+set(gcf,'Units','inches');
+set(gcf,'Position',[v(1) v(2) w h]);
+
+p=zeros(1,Nt);
+
+subplot_tight(1,2,1:2,[.1 .07])
+set(gca,'FontSize',labfontsz)
 hold on
-plot(threshvals2,threshvals2,'k','LineWidth',3)
-[half,~]=contour(threshvals2,threshvals2,transpose(twomat(:,:,1)),.5);
-half=dividecontours(half);
-half=half{1};
-plot(half(1,:),half(2,:),'k','LineWidth',3)
+
+for i=1:Nr
+    p(i)=plot(reshape(twoflexthresh(i,:,2),1,[]),reshape(twoflexthresh(i,:,1),1,[]),'Color',blue(Nr-i+2,:),'LineWidth',lw);
+end
+
+ylabel('Probability of correct output','FontSize',textfontsz)
+xlabel('Expected time to output','FontSize',textfontsz)
+
+legendlabels={};
+for i=1:Nr
+    legendlabels(i)=cellstr(['\rho=' num2str(ratios(i))]);
+end
+
+% leg=legend(p(1:5),['d=' num2str(ratios(1))],['d=' num2str(ratios(2))],['d=' num2str(ratios(3))],['d=' num2str(ratios(4))],['d=' num2str(ratios(5))]);
+leg=legend(p,legendlabels);
+legend boxoff
+% leg_line=findobj(leg,'type','Line');
+% for i = 1:Nr
+%      set(leg_line(2*i), 'Color', black(i+1,:));
+%      set(leg_line(2*i-1),'Color',black(i+1,:));
+% end
+
+set(leg,'Position',[.85 .75 .1 .1])
+    
+set(gcf,'PaperSize',[w h]);
+set(gcf,'PaperPosition',[0 0 w h]);
+
+filename=strcat('/Users/eleanorbrush/Dropbox/signaling_network/','2d_flexible_thresholds','.pdf');
+print(filename,'-dpdf','-r300');
